e1ba44e6f776ee515e24a640a7d3544174f2d2b9
Maya <evamayadila09@gmail.com>
2023-04-21 13:44:56 +0700

    Update beranda FAQ

---
+++ Object Repository/FAQS/Beranda FAQ/div_List FAQs BliCicil Admin as            _3c8d92.rs
@@ -0,0 +1,118 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_List FAQs BliCicil Admin as            _3c8d92</name>
+   <tag></tag>
+   <elementGuidId>61bb3c71-d6cd-49b7-88a0-af25cb9d2aa5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.card-header.border-bottom.p-1</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//section[@id='complex-header-datatable']/div/div/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>789af2f1-54e7-4978-a970-854555fbd9c5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>card-header border-bottom p-1</value>
+      <webElementGuid>351ce2c3-6b1b-4e3b-bee8-ce1408008560</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                                
+                                    List FAQs BliCicil Admin as
+                                
+                                
+                                    
+                                        New FAQ
+                                    
+                                
+                            </value>
+      <webElementGuid>7b875413-ed1d-4101-98aa-c6a6465ec2d5</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;complex-header-datatable&quot;)/div[@class=&quot;row&quot;]/div[@class=&quot;col-12&quot;]/div[@class=&quot;card&quot;]/div[@class=&quot;card-header border-bottom p-1&quot;]</value>
+      <webElementGuid>b46a7498-6eec-4749-99bb-82597845c474</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//section[@id='complex-header-datatable']/div/div/div/div</value>
+      <webElementGuid>4d460c7b-65f7-4b5c-ad6a-490ab2194214</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='FAQs'])[1]/following::div[5]</value>
+      <webElementGuid>4b7cbd35-9885-41ad-b538-dec17e5ad56b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[1]/following::div[5]</value>
+      <webElementGuid>821861bf-96cc-45e1-ad02-c807c0db8876</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div/div/div/div</value>
+      <webElementGuid>76dcfefc-6ebf-45a8-b095-6423b3655dc9</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+                                
+                                    List FAQs BliCicil Admin as
+                                
+                                
+                                    
+                                        New FAQ
+                                    
+                                
+                            ' or . = '
+                                
+                                    List FAQs BliCicil Admin as
+                                
+                                
+                                    
+                                        New FAQ
+                                    
+                                
+                            ')]</value>
+      <webElementGuid>fa8918a4-679c-4060-8899-cbdeb2a58ad8</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
